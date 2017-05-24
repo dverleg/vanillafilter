2f5538fdad6482efa443daaf3703df496a5810ae
Dennis Verleg <dennis@dtcmedia.nl>
2017-05-24 14:01:51 +0200

    fix(test): New test added, sinon added to tests

--- test/index.tests.js
+++ test/index.tests.js
@@ -1,14 +1,43 @@
 var chai = require('chai');
+var sinon = require('sinon');
+var sinonChai = require('sinon-chai');
+chai.should();
+chai.use(sinonChai);
+
 var assert = chai.assert;
 var expect = chai.expect;
 var vf = require('../src/scripts/vanillafilter');
 
 describe('VanillaFilter', function() {
 	it('should create a VanillaFilter instance with an "options" property', function() {
-		var VF = new VanillaFilter();
+		var VFoptionsObject = new VanillaFilter();
 
-		assert.isObject(VF, 'VanillaFilter instance is an object');
-		assert.isObject(VF.options, 'VanillaFilter.options is an object');
+		assert.isObject(VFoptionsObject, 'VanillaFilter instance is an object');
+		assert.isObject(VFoptionsObject.options, 'VanillaFilter.options is an object');
 	});
-});
 
+	it('should have a bind function', function() {
+		var VFbindFn = new VanillaFilter();
+
+		assert.isFunction(VFbindFn.bind, 'VanillaFilter.bind exists and is a function');
+	});
+
+	it('should have a triggerFilter function', function() {
+		var VFtriggerFilterFn = new VanillaFilter();
+
+		assert.isFunction(VFtriggerFilterFn.triggerFilter, 'VanillaFilter.triggerFilter exists and is a function');
+	});
+
+	it('should override user input options', function() {
+		var VFdefaultOptions = new VanillaFilter();
+		var defaultOptions = VFdefaultOptions.options;
+		var VFcustomOptions = new VanillaFilter({
+			'vanillaDisplayType': 'flex'
+		});
+		var customOptions = VFcustomOptions.options;
+
+		expect(defaultOptions).to.not.equal(customOptions);
+	});
+
+
+});
